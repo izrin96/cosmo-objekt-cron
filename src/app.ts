194de59d06b5d632cc0509556a9309a252949701
Shahredza <izrin96@gmail.com>
2025-08-27 17:27:38 +0800

    disable sco transferable check

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cron from "node-cron";
 import { updateTransferableCosmoSpin } from "./cron/cosmo-spin";
 import { fixCollection, fixObjektSerial } from "./cron/collection";
-import { updateTransferableScoGridAll } from "./cron/sco-grid-all";
-import { updateTransferableScoGrid } from "./cron/sco-grid";
+// import { updateTransferableScoGridAll } from "./cron/sco-grid-all";
+// import { updateTransferableScoGrid } from "./cron/sco-grid";
 
 // At every minute
 cron.schedule("* * * * *", async () => {
@@ -10,10 +10,10 @@ cron.schedule("* * * * *", async () => {
 });
 
 // At minute 0 past every 2nd hour
-cron.schedule("0 */2 * * *", async () => {
-  // todo: to remove or check as its not accurate, maybe modhaus has some delay on transferable update
-  await updateTransferableScoGrid();
-});
+// cron.schedule("0 */2 * * *", async () => {
+//   // todo: to remove or check as its not accurate, maybe modhaus has some delay on transferable update
+//   await updateTransferableScoGrid();
+// });
 
 // At 00:00
 cron.schedule("0 0 * * *", async () => {
@@ -22,6 +22,6 @@ cron.schedule("0 0 * * *", async () => {
 });
 
 // At 00:00 on Friday
-cron.schedule("0 0 * * 5", async () => {
-  await updateTransferableScoGridAll();
-});
+// cron.schedule("0 0 * * 5", async () => {
+//   await updateTransferableScoGridAll();
+// });
